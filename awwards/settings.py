b9f358f379aff64aa527563f67be31c13a466b11
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-06-11 17:04:33 +0300

    create database and set its configurations

--- awwards/settings.py
+++ awwards/settings.py
@@ -80,8 +80,11 @@ WSGI_APPLICATION = 'awwards.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST' : config('DB_HOST'),
     }
 }
 
