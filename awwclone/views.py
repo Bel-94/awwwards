36beea55364c274dd62603cca8720f112d60cc72
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-06-12 21:17:37 +0300

    create a templates directories and its sub-folders

--- awwclone/views.py
+++ awwclone/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404
+from . models import Profile, Project, Rateview
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from .forms import profileForm,UserUpdateForm,RegistrationForm,projectForm,UpdateUserProfileForm,RateForm
+
 
 # Create your views here.
 def Home(request):
