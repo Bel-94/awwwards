994423407cc019033ca8a9762ee51bc8d1685531
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-06-13 09:18:07 +0300

    create the urls

--- awwclone/urls.py
+++ awwclone/urls.py
@@ -1,6 +1,15 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='Home'),
+    path('',views.index, name = 'home'),
+    path('search/', views.searchprofile, name='search'),
+    path('newproject/',views.addProject,name = 'project'),
+    path('profile/<id>/',views.profile,name = 'profile'),
+    path('editprofile/',views.editprofile,name = 'editprofile'), 
+    path('api/profile',views.ProfileList.as_view()),
+    path('api/projects',views.ProjectList.as_view()),
+    path('projects/<id>/',views.projects,name = 'projects'),
+    path('ratings/', include('star_ratings.urls', namespace='ratings')),
+    path('rate/<id>/',views.rate,name = 'rate')
 ]
