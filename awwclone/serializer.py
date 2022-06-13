25760c577b0c54376464be95c07e7dd27f174257
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-06-13 09:02:01 +0300

    update the serializer file

---
+++ awwclone/serializer.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from . models import Profile, Project
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ('bio', 'photo', 'user')
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ('title', 'description', 'projectscreenshot','projecturl','user')     
+
+
